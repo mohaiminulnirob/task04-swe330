0027d43d80804416e4c44ad4cdc72a166dcc7102
mohaiminulnirob <mohaiminulislam20000@gmail.com>
2025-09-05 00:23:52 +0600

    update .env and config/db.js file to connect with xampp mysql database

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
-// uses mysql2/promise for async/await
 const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'task_manager',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
